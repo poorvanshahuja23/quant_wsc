f769737bbe554ccd1d1881b46cd137878fa441ea
Poorvansh_Ahuja <poorvanshahuja23@gmail.com>
2026-09-05 23:30:58 +0530

    Clean up repository to keep only PDF and gitignore

--- quant_research_engine/strategies/__init__.py
+++
@@ -1,4 +0,0 @@
-from .mean_reversion import run_mean_reversion
-from .trend_following import run_trend_following
-from .pairs_trading import run_pairs_trading
-from .volatility_breakout import run_volatility_breakout
